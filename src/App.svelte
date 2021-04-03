3390aaf4c1ee3f9e30c1301a45e055052bae3813
Aurel <aurelien.catinon@gmail.com>
2021-04-03 15:40:18 +0200

    WIP: connecting to web3

--- src/App.svelte
+++ src/App.svelte
@@ -1,70 +1,89 @@
 <script lang="typescript">
   import { onMount } from "svelte";
-  import { decimal } from "./utils" 
+  import { decimal, web3 } from "./utils";
 
-  import Expansion from './components/Expansion.svelte';
-  import FormattedDecimalInput from './components/FormattedDecimalInput.svelte';
+  import Expansion from "./components/Expansion.svelte";
+  import FormattedDecimalInput from "./components/FormattedDecimalInput.svelte";
 
   const twap = decimal(1);
   const totalSupply = decimal(141_593_462);
   const bondedDsd = decimal(17_835_130);
   const bondedLp = decimal(3_578_883);
   const bondedCdsd = decimal(5_000_000);
-
+  const web3Provider = web3();
 </script>
 
-<section class="hero">
-  <div class="hero-body">
-    <h1 class="title">Welcome to DSD Simulator!</h1>
-  </div>
-</section>
+<div class="container">
+  <nav class="level section">
+    <div class="level-left">
+      <div class="level-item">
+        <p class="subtitle is-5">DSD Simulator</p>
+      </div>
+    </div>
+    <div class="level-right">
+      <p class="level-item">
+        {#if $web3Provider.isConnected}
+          {#await $web3Provider.provider.listAccounts()}
+            ...
+          {:then accounts}
+            <span class="tag is-primary is-light">{accounts[0]}</span>
+          {/await}
+        {:else}
+          <button class="button is-primary" on:click={web3Provider.connect}>Connect</button>
+        {/if}
+      </p>
+    </div>
+  </nav>
 
-<section class="section">
-  <h2 class="subtitle">Settings</h2>
-  <form class="is-flex is-flex-direction-row">
-    <div class="field mr-2">
-      <label class="label">TWAP</label>
-      <div class="control">
-        <FormattedDecimalInput store={twap} />
+  <section class="section">
+    <h2 class="subtitle">Settings</h2>
+    <form
+      class="is-flex is-flex-direction-row is-justify-content-space-between"
+    >
+      <div class="field mr-2">
+        <label class="label">TWAP</label>
+        <div class="control">
+          <FormattedDecimalInput store={twap} />
+        </div>
       </div>
-    </div>
-    <div class="field mr-2">
-      <label class="label">Total supply</label>
-      <div class="control">
-        <FormattedDecimalInput store={totalSupply} />
+      <div class="field mr-2">
+        <label class="label">Total supply</label>
+        <div class="control">
+          <FormattedDecimalInput store={totalSupply} />
+        </div>
       </div>
-    </div>
-    <div class="field mr-2">
-      <label class="label">Bonded DSD</label>
-      <div class="control">
-        <FormattedDecimalInput store={bondedDsd} />
+      <div class="field mr-2">
+        <label class="label">Bonded DSD</label>
+        <div class="control">
+          <FormattedDecimalInput store={bondedDsd} />
+        </div>
       </div>
-    </div>
-    <div class="field mr-2">
-      <label class="label">Bonded LP</label>
-      <div class="control">
-        <FormattedDecimalInput store={bondedLp} />
+      <div class="field mr-2">
+        <label class="label">Bonded LP</label>
+        <div class="control">
+          <FormattedDecimalInput store={bondedLp} />
+        </div>
       </div>
-    </div>
-    <div class="field mr-2">
-      <label class="label">Bonded CDSD</label>
-      <div class="control">
-        <FormattedDecimalInput store={bondedCdsd} />
+      <div class="field mr-2">
+        <label class="label">Bonded CDSD</label>
+        <div class="control">
+          <FormattedDecimalInput store={bondedCdsd} />
+        </div>
       </div>
-    </div>
-  </form>
-</section>
+    </form>
+  </section>
 
-<section class="section">
-  <h2 class="subtitle">
-    Next epoch
-    {#if $twap > 1}
-      <span class="tag is-light is-success">Expansion</span>
-    {/if}
-    {#if $twap < 1}
-      <span class="tag is-light is-danger">Contraction</span>
-    {/if}
-  </h2>
+  <section class="section">
+    <h2 class="subtitle">
+      Next epoch
+      {#if $twap > 1}
+        <span class="tag is-light is-success">Expansion</span>
+      {/if}
+      {#if $twap < 1}
+        <span class="tag is-light is-danger">Contraction</span>
+      {/if}
+    </h2>
 
-  <Expansion twap={twap} totalSupply={totalSupply} bondedDsd={bondedDsd} bondedLp={bondedLp} bondedCdsd={bondedCdsd}/>
-</section>
+    <Expansion {twap} {totalSupply} {bondedDsd} {bondedLp} {bondedCdsd} />
+  </section>
+</div>
--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,92 @@
 import { writable, derived, Readable, Writable } from "svelte/store";
 import BigNumber from "bignumber.js";
+import Web3Modal from "web3modal";
+import { ethers } from "ethers";
 
 export interface DecimalStore extends Writable<number> {
-    asBig(): Readable<BigNumber>
+	asBig(): Readable<BigNumber>;
 }
 
+export interface Web3Store extends Readable<Web3Provider> {
+	connect(): void;
+	disconnect(): void;
+}
+
+export interface Web3Provider {
+	provider?: ethers.providers.Web3Provider;
+	chainId?: number;
+	isConnected: boolean;
+}
+
+export function web3(): Web3Store {
+	const providerOptions = {
+		/* See Provider Options Section */
+	};
+
+	const web3Modal = new Web3Modal({
+		network: "mainnet",
+		cacheProvider: true,
+		providerOptions
+	});
+
+	const init: Web3Provider = {
+		provider: undefined,
+		chainId: undefined,
+		isConnected: false
+	};
+
+	const { subscribe, set, update } = writable(init);
+
+	web3Modal.on("connect", (info: { chainId: number }) => {
+		update(web3 => {
+			web3.chainId = info.chainId;
+			return web3;
+		});
+	});
+
+	web3Modal.on("disconnect", (error: { code: number; message: string }) => {
+		update(web3 => {
+			web3.isConnected = false;
+			web3.chainId = undefined;
+			return web3;
+		});
+	});
+
+	return {
+		subscribe,
+		connect: () => {
+			web3Modal.connect().then((provider) => {
+				update(web3 => {
+					web3.provider = new ethers.providers.Web3Provider(provider);
+					web3.isConnected = true;
+					return web3;
+				});
+			});
+		},
+		disconnect: () => {
+			update(web3 => {
+				web3.provider = undefined;
+				web3.chainId = undefined;
+				web3.isConnected = false;
+				return web3;
+			});
+			web3Modal.clearCachedProvider();
+		}
+	}
+}
 
 export function decimal(init: number): DecimalStore {
 	const store = writable(init);
-    const bigStore = derived(store, $store => new BigNumber($store))
+	const bigStore = derived(store, $store => new BigNumber($store))
 
 	return {
 		subscribe: store.subscribe,
 		set: (newValue: number) => {
-			if (newValue != null)
-			{
+			if (newValue != null) {
 				store.set(newValue);
 			}
 		},
 		update: store.update,
-        asBig: () => bigStore
+		asBig: () => bigStore
 	};
 }
